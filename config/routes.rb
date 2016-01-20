9dfb9e0f1aff5706d15df29ebc325d69c1a5b397
rosecajobe <roseannecjb@gmail.com>
2016-01-20 18:18:46 +0800

    - show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :savings, only: [:index, :new, :create]
+  resources :savings, only: [:index, :new, :create, :show]
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
